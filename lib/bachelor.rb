b2c22cc0201d009628f27a51f9c23d8e23069c89
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-09 17:29:40 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,16 @@
 def get_first_name_of_season_winner(data, season)
- 
+ answer = nil 
+ data.each do |seasons, array_contestants|
+   if seasons = season
+     array_contestants.each do |each_cont_details|
+       if each_cont_details[:status] == "Winner"
+         answer = each_cont_details[:name]
+       end
+     end
+    end
+  end
+    first_name_only = answer.split(" ")
+    first_name_only[0]
 end
 
 def get_contestant_name(data, occupation)
